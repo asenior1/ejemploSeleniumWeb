e63a399d29fbc68f8147384de8c76688bf1aa5f0
asenior1 <senior.andrea1@gmail.com>
2021-01-25 22:29:58 -0300

    Update AppTest.java

--- src/test/java/selenium/AppTest.java
+++ src/test/java/selenium/AppTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
